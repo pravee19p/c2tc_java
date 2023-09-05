d1e01441e4d6d23298c948ef57567d6d993f359e
USER <USER@DESKTOP-AQI90ID>
2023-09-05 09:57:05 +0530

    first commit

---
+++ javaprogram/src/com/tns/overloading/OverloadingDemo.java
@@ -0,0 +1,18 @@
+package com.tns.overloading;
+
+public class OverloadingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point p=new Point();
+		System.out.println(p);
+		Point p1=new Point(0.2f);
+		System.out.println(p1);
+		Point p2=new Point(0.3f,0.9f);
+		System.out.println(p2);
+		
+		
+		
+			
+}
+}
---
+++ javaprogram/src/com/tns/overloading/Point.java
@@ -0,0 +1,42 @@
+package com.tns.overloading;
+
+public class Point {
+
+	private float x;
+	private float y;
+	public  Point()
+	{
+		x=0.0f;
+		y=0.0f;
+	}
+	public  Point(float x)
+	{
+		this.x=x;
+		this.y=x;
+	}
+	public  Point(float x,float y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+	public float getX() {
+		return x;
+	}
+	public void setX(float x) {
+		this.x = x;
+	}
+	public float getY() {
+		return y;
+	}
+	public void setY(float y) {
+		this.y = y;
+	}
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+	
+	
+}
+
+
