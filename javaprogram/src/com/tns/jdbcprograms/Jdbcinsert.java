942ecedd02fa3c5f97b4884dad2e5902728a183b
USER <USER@DESKTOP-AQI90ID>
2023-10-09 21:05:02 +0530

    first commit

---
+++ javaprogram/src/com/tns/jdbcprograms/Jdbcinsert.java
@@ -0,0 +1,42 @@
+package com.tns.jdbcprograms;
+import java.sql.*;
+import javax.sql.*;
+
+public class Jdbcinsert {
+	public static void main(String[]args) {
+		try {
+		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/c21", "root", "prg@123");
+		
+		//create the statement
+		Statement stmt=conn.createStatement();
+		//execute a sql insert statement via executeupdate()
+//		String sqlInsert="insert into books values(132,'c',499,2)";
+//		System.out.println("the sql statement is "+sqlInsert);
+//		int countInserted=stmt.executeUpdate(sqlInsert);
+//		System.out.println(countInserted +" records inserted");
+		//delete the record
+//		String sqldelete="delete from books where bookid=128";
+//		System.out.println("the sql statement is "+sqldelete);
+//		int countdeleted=stmt.executeUpdate(sqldelete);
+//		System.out.println(countdeleted +" records deleted");
+		//display records
+		String strselect="select * from books";
+		System.out.println("the sql statement is "+strselect);
+		ResultSet rst=stmt.executeQuery(strselect);
+		System.out.println(" records are: ");
+		int rowcount=0;
+		while(rst.next()) {
+		
+			System.out.println(rst.getInt("bookid"));
+			System.out.println(rst.getString("booktitle"));
+			System.out.println(rst.getInt("price"));
+			System.out.println(rst.getInt("qty"));
+			++rowcount;
+		}
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();		}
+	}
+
+}
