e6d6683eb012389e9ffd1bf0f40a39d65481f23f
prave <prave@dell19>
2023-11-27 20:32:43 +0530

    first commit

--- shoppingmalladmin/src/test/java/com/shoppingmall/ShoppingmalladminApplicationTests.java
+++ shoppingmalladmin/src/test/java/com/shoppingmall/ShoppingmalladminApplicationTests.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +20,7 @@ class ShoppingmalladminApplicationTests {
 	private AdminRepository rep;
 	
 	@Test
+	@DisplayName("Find all test")
 	public void testall() {
 		
 		List<Admin> list=rep.findAll();	
